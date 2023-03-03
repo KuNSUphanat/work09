8e0375a059a7dd79106b14757ddf5f2e1caca52f
kunsuphanat <errorisaki00@gmail.com>
2023-03-03 22:11:42 +0700

    project init

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB7nKGGvjdDsr66bPIc40ClYkNVgRHh7ck",
+  authDomain: "lexical-pathway-360606.firebaseapp.com",
+  projectId: "lexical-pathway-360606",
+  storageBucket: "lexical-pathway-360606.appspot.com",
+  messagingSenderId: "1002133486078",
+  appId: "1:1002133486078:web:5b1708c2dc0eca48e1e8af",
+  measurementId: "G-TB8ZSVCX30"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
